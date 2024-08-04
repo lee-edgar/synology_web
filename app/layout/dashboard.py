e7d8d747c48784cafae7aba8f5384ba30cd6478b
lee-edgar <61241244+lee-edgar@users.noreply.github.com>
2024-08-04 16:34:00 +0900

    sidebar layout 수정

--- app/layout/dashboard.py
+++ app/layout/dashboard.py
@@ -22,7 +22,7 @@ class DashLayout:
         st.markdown(f'### Update Time = {timedelta(seconds=etime - stime)}')
 
     def draw_sidebar(self):
-        st.header("전설의 김박펭귄 모험가의 여정을 담은 페이지입니다.")
+        # st.header("전설의 김박펭귄 모험가의 여정을 담은 페이지입니다.")
         # st.image('image/kimparkpenguin.png', use_column_width='auto')
 
         # if 'pdf_ref' not in ss:
@@ -34,44 +34,64 @@ class DashLayout:
         #     binary_data = ss.pdf_ref.getvalue()
         #     pdf_viewer(input=binary_data, width=700)
 
-        options = list(Menu)
-        tabs = on_hover_tabs(
-            tabName=[menu.value for menu in options],
-            iconName=['list'] * len(options),
-            default_choice=2,
-            styles=get_sidebar_styles()
+        # options = list(Menu)
+        # tabs = on_hover_tabs(
+        #     tabName=[menu.value for menu in options],
+        #     iconName=['list'] * len(options),
+        #     default_choice=2,
+        #     styles=get_sidebar_styles()
+        #
+        # )
+        #
+        # if tabs == Menu.study_group.value:
+        #     study_options = list(StudyGroup)
+        #     study_tabs = on_hover_tabs(
+        #         tabName=[menu.value for menu in study_options],
+        #         iconName=['list'] * len(study_options),
+        #         default_choice=0,
+        #         styles=get_sidebar_styles()
+        #
+        #     )
+        #     # 여기에 study_group 탭의 컨텐츠를 추가할 수 있습니다.
+        # elif tabs == Menu.story_group.value:
+        #     story_options = list(StoryGroup)
+        #     story_tabs = on_hover_tabs(
+        #         tabName=[menu.value for menu in story_options],
+        #         iconName=['list'] * len(story_options),
+        #         default_choice=0,
+        #         styles=get_sidebar_styles()
+        #
+        #     )
+        #     # 여기에 story_group 탭의 컨텐츠를 추가할 수 있습니다.
+        # elif tabs == Menu.profile_group.value:
+        #     profile_options = list(ProfileGroup)
+        #     profile_tabs = on_hover_tabs(
+        #         tabName=[menu.value for menu in profile_options],
+        #         iconName=['list'] * len(profile_options),
+        #         default_choice=0,
+        #         styles=get_sidebar_styles()
+        #     )
+        #     # 여기에 profile_group에 대한 코드를 작성할 수 있습니다.
+        #     pass
 
-        )
+        st.sidebar.header("메뉴")
 
-        if tabs == Menu.study_group.value:
-            study_options = list(StudyGroup)
-            study_tabs = on_hover_tabs(
-                tabName=[menu.value for menu in study_options],
-                iconName=['list'] * len(study_options),
-                default_choice=0,
-                styles=get_sidebar_styles()
+        # Dictionary to map main categories to their subcategories
+        submenu_mapping = {
+            Menu.study_group: list(StudyGroup),
+            Menu.story_group: list(StoryGroup),
+            Menu.profile_group: list(ProfileGroup)
+        }
 
-            )
-            # 여기에 study_group 탭의 컨텐츠를 추가할 수 있습니다.
-        elif tabs == Menu.story_group.value:
-            story_options = list(StoryGroup)
-            story_tabs = on_hover_tabs(
-                tabName=[menu.value for menu in story_options],
-                iconName=['list'] * len(story_options),
-                default_choice=0,
-                styles=get_sidebar_styles()
+        # Initialize session state for menu
+        if 'active_tab' not in st.session_state:
+            st.session_state.active_tab = None
 
-            )
-            # 여기에 story_group 탭의 컨텐츠를 추가할 수 있습니다.
-        elif tabs == Menu.profile_group.value:
-            profile_options = list(ProfileGroup)
-            profile_tabs = on_hover_tabs(
-                tabName=[menu.value for menu in profile_options],
-                iconName=['list'] * len(profile_options),
-                default_choice=0,
-                styles=get_sidebar_styles()
-            )
-            # 여기에 profile_group에 대한 코드를 작성할 수 있습니다.
-            pass
+        # Display main categories with default expanded state
+        for menu in Menu:
+            with st.sidebar.expander(menu.value, expanded=True):
+                for submenu in submenu_mapping[menu]:
+                    if st.button(submenu.value, key=f"{menu.value}-{submenu.value}"):
+                        st.session_state.active_tab = submenu.value
 
 dashboard_layout: DashLayout = DashLayout()
