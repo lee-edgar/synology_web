afbef957819d755fcd348549c14062ff5d9608e1
lee-edgar <61241244+lee-edgar@users.noreply.github.com>
2024-11-07 22:21:41 +0900

    프로필 경력 사항에 대해서 expander 적용(펼쳐진 expander가 디폴트가 되게 설정)

--- app/layout/profile_layout.py
+++ app/layout/profile_layout.py
@@ -22,65 +22,68 @@ class Profile:
                 careers = response.json()
                 st.title("🏢 경력 사항")
                 for career in careers:
-                    with st.container():
-                        col1, col2 = st.columns([3, 1])
-                        with col1:
-                            # 회사명과 직책
-                            st.subheader(f"{career['title']}")
-                            st.write(f"🏢 **{career['company']}**  •  {career['company_type']}  •  {career['location']}")
-                        with col2:
-                            # 근무 기간
-                            period = f"{career['start_date']} ~ {career['end_date'] if career['end_date'] else '현재'}"
-                            st.caption(f"🗓️ {period}")
-                        # 업무 설명
-                        if career.get('description'):
-                            st.markdown(career['description'])
+                    with st.expander(f"{career['title']}", expanded=True):
+                        with st.container():
+                            col1, col2 = st.columns([3, 1])
+                            with col1:
+                                # 회사명과 직책
+                                st.subheader(f"{career['title']}")
+                                st.write(f"🏢 **{career['company']}**  •  {career['company_type']}  •  {career['location']}")
+                            with col2:
+                                # 근무 기간
+                                period = f"{career['start_date']} ~ {career['end_date'] if career['end_date'] else '현재'}"
+                                st.caption(f"🗓️ {period}")
+                            # 업무 설명
+                            if career.get('description'):
+                                st.markdown(career['description'])
 
-                        if career.get('files'):
-                            try:
-                                files = career.get('files')
-                                # 파일 이름과 타입 추출
-                                file_name = files[0]
-                                file_type = files[1]
-                                file_caption = files[2] if len(files) > 2 else None
+                            if career.get('files') and career.get('files') != ["[]"]:
+                                # st.write("Files content:", career.get('files'))  # 디버깅용
 
-                                # 파일 타입에 따라 처리
-                                if file_type == 'image':
-                                    if file_caption is None:
-                                        st.image(file_name, use_column_width='always')
-                                    else:
-                                        st.image(file_name, caption=file_caption, use_column_width='always')
-                                elif file_type == 'pdf':
-                                    st.write("📄 관련 문서")
-                                    col1, col2 = st.columns([3, 1])
-                                    with col1:
-                                        st.write(f"• {file_name}")
-                                    with col2:
-                                        st.download_button(
-                                            label="다운로드",
-                                            data=file_name,  # 실제 파일 데이터를 전달해야 함
-                                            file_name=file_name,
-                                            mime="application/pdf"
-                                        )
-                            except json.JSONDecodeError as e:
-                                st.error(f"파일 정보 파싱 오류: {str(e)}")
+                                try:
+                                    files = career.get('files')
+                                    # 파일 이름과 타입 추출
+                                    file_name = files[0]
+                                    file_type = files[1]
+                                    file_caption = files[2] if len(files) > 2 else None
 
-                        # 태그 표시
-                        if career.get('tags'):
-                            try:
-                                tags = career.get('tags')
-                                max_columns = 1
-                                for i in range(0, len(tags), max_columns):
-                                    cols = st.columns(max_columns)
-                                    for idx, tag in enumerate(tags[i:i + max_columns]):
-                                        with cols[idx]:
-                                            st.markdown(
-                                                f"<span style='background-color: #f0f2f6; margin:left; padding: 2px 8px; border-radius: 12px;'>#{tag}</span>",
-                                                unsafe_allow_html=True
+                                    # 파일 타입에 따라 처리
+                                    if file_type == 'image':
+                                        if file_caption is None:
+                                            st.image(file_name, use_column_width='always')
+                                        else:
+                                            st.image(file_name, caption=file_caption, use_column_width='always')
+                                    elif file_type == 'pdf':
+                                        st.write("📄 관련 문서")
+                                        col1, col2 = st.columns([3, 1])
+                                        with col1:
+                                            st.write(f"• {file_name}")
+                                        with col2:
+                                            st.download_button(
+                                                label="다운로드",
+                                                data=file_name,  # 실제 파일 데이터를 전달해야 함
+                                                file_name=file_name,
+                                                mime="application/pdf"
                                             )
-                            except json.JSONDecodeError as e:
-                                st.error(f"태그 파싱 오류: {str(e)}")
-                            st.write("---")  # 구분선
+                                except json.JSONDecodeError as e:
+                                    st.error(f"파일 정보 파싱 오류: {str(e)}")
+
+                            # 태그 표시
+                            if career.get('tags'):
+                                try:
+                                    tags = career.get('tags')
+                                    max_columns = 1
+                                    for i in range(0, len(tags), max_columns):
+                                        cols = st.columns(max_columns)
+                                        for idx, tag in enumerate(tags[i:i + max_columns]):
+                                            with cols[idx]:
+                                                st.markdown(
+                                                    f"<span style='background-color: #f0f2f6; margin:left; padding: 2px 8px; border-radius: 12px;'>#{tag}</span>",
+                                                    unsafe_allow_html=True
+                                                )
+                                except json.JSONDecodeError as e:
+                                    st.error(f"태그 파싱 오류: {str(e)}")
+                                st.write("---")  # 구분선
             else:
                 st.error(f"데이터를 불러오는데 실패했습니다. (Status: {response.status_code})")
                 if response.text:
