49deaaea333a0e673e831ecd28bd1dbfb1c47a83
lee-edgar <61241244+lee-edgar@users.noreply.github.com>
2024-11-10 00:13:18 +0900

    streamlit pdf view 업데이트

--- app/layout/portfolio_layout.py
+++ app/layout/portfolio_layout.py
@@ -4,6 +4,7 @@ import json
 from app.common.common import BACKEND_URL
 from app.utils.singleton import  singleton
 from app.layout.const import *
+from streamlit_pdf_viewer import pdf_viewer
 
 
 class Portfolio:
@@ -12,9 +13,20 @@ class Portfolio:
         pass
 
     def draw(self):
+        # col1, col2, col3 = st.columns([2, 8, 1])  # 8:2:1 비율
         st.markdown(f"{PORTFOLIO_VIEW_MARKDOWN}")
         self.draw_portfolio_group_view()
 
     def draw_portfolio_group_view(self):
         st.image('penguin_mov.gif', use_column_width='auto')
 
+        pdf_path = '버섯분류프로젝트.pdf'
+        pdf_viewer(
+            pdf_path,
+            width=1000,
+            height=1000,
+            render_text=True,
+            pages_vertical_spacing=2,
+            scroll_to_page=3,
+            annotation_outline_size=2
+        )
