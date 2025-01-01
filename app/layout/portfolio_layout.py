164f94f3b5575d0edefe4feab227aadfca7fb70a
lee-edgar <61241244+lee-edgar@users.noreply.github.com>
2025-01-02 00:57:50 +0900

    채널헬스케어 포트폴리오 추가 작성

--- app/layout/portfolio_layout.py
+++ app/layout/portfolio_layout.py
@@ -25,6 +25,18 @@ class Portfolio:
         # st.markdown('PortfolioGroup', PortfolioGroup.retinal_oct_disease_classification)
     def draw_portfolio_group_view(self):
         # st.image('penguin_mov.gif', use_column_width='auto')
+        with st.expander(" 채널헬스케어 포트폴리오", expanded=True):
+            pdf_path = '채널헬스케어 포트폴리오.pdf'
+            pdf_viewer(
+                pdf_path,
+                width=1300,
+                height=1000,
+                render_text=True,
+                pages_vertical_spacing=1,
+                scroll_to_page=3,
+                annotation_outline_size=2
+            )
+
         with st.expander(" 버섯분류프로젝트 ", expanded=True):
             pdf_path = '버섯분류프로젝트.pdf'
             pdf_viewer(
